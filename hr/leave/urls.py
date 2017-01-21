5fa7fea0fca80117959a10c5edfd2d3c664141d3
Brandon <phillipsb.22@gmail.com>
2017-01-21 16:15:16 +0200

    Adding new files as well committing templates. Battling with the templates keeps telling me that the template does not exist.

---
+++ hr/leave/urls.py
@@ -0,0 +1,11 @@
+
+from django.conf.urls import url
+
+from . import views
+from . import login_view
+
+urlpatterns = [
+  url(r'^$', views.index, name='index'),
+  url(r'^test/$', views.test, name='test'),
+  url(r'^login/$', views.login, name='login'),
+]
--- hr/leave/views.py
+++ hr/leave/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+#import inspect,os
 
 
 def index(request):
     return HttpResponse("Test http response")
 
 # Create your views here.
+
+def test(request):
+	return HttpResponse("Gets to view 2 to make sure")
+
+def login(request):
+    #return HttpResponse(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+    template = loader.get_template('leave/login.html')
+    return HttpResponse(template.render(request))
+
+
