03aab7c28591835025d8fd48f61ff42758622ab7
Brandon <phillipsb.22@gmail.com>
2017-01-23 21:54:15 +0200

    BP - committing getting the login verify function to route correctly

--- hr/leave/urls.py
+++ hr/leave/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
   url(r'^$', views.index, name='index'),
   url(r'^test/$', views.test, name='test'),
   url(r'^login/$', views.login, name='login'),
-  url(r'^/verify/$', views.validate_login, name='validate_login'),
+  url(r'^verify/$', views.validate_login, name='validate_login'),
 ]
